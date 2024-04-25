3b8f7136b778953f811685ad635cab811a92f89b
zwxdogs <13023523320@163.com>
2024-04-25 09:18:36 +0800

    study

--- CSAPPStudy/StudyTest_cpp/src/main.cpp
+++ CSAPPStudy/StudyTest_cpp/src/main.cpp
@@ -1,9 +1,9 @@
+#include <climits>
+#include <cstdint>
+#include <cstdio>
 #include <iostream>
 
 using namespace std;
+using byte_pointer = unsigned char*;
 
-int main(int argc, char** argv)
-{
-    cout << "hello world!" << endl;
-    return 0;
-}
+int main(int argc, char** argv) { return 0; }
